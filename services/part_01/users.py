a827d7aea0c26e0c1897376abcacb4ec6d50c343
MarcinLukasik <126354301+Cytrynio@users.noreply.github.com>
2025-06-28 13:01:49 +0200

    Update users.py

--- services/part_01/users.py
+++ services/part_01/users.py
@@ -1,10 +1,16 @@
 import requests
+API_KEY = "reqres-free-v1"
+
+HEADERS = {
+    "Authorization": f"Bearer {API_KEY}"
+}
+
 
 def get_user(id):
     url = f"https://reqres.in/api/users/{id}"
 
     session = requests.session()
-    response = session.get(url)
+    response = session.get(url,headers=HEADERS)
 
     return response
 
